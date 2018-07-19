6ebe7495a52bf4e3143510738429f6f830fae479
eagobert <eagobert@hotmail.com>
2018-07-19 16:16:07 -0500

    add templates

--- music/history/models.py
+++ music/history/models.py
@@ -5,8 +5,8 @@ class Artist(models.Model):
     artist_name = models.CharField(max_length=30)
     year_est = models.CharField(max_length=4)
 
-    def __str__(self):
-        return 'Name: %s, Year Established: %s' % (self.artist_name, self.year_est)
+    # def __str__(self):
+    #     return 'Name: %s, Year Established: %s' % (self.artist_name, self.year_est)
 
 class Song(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
@@ -14,5 +14,5 @@ class Song(models.Model):
     song_length = models.IntegerField(default=0)
     release_date = models.DateField(auto_now=False, auto_now_add=False)
 
-    def __str__(self):
-        return 'Song Title: %s, Release Date: %s, Song Duration: %s' % (self.title, self.release_date, self.song_length )
+    # def __str__(self):
+    #     return 'Song Title: %s, Release Date: %s, Song Duration: %s' % (self.title, self.release_date, self.song_length )
--- music/history/views.py
+++ music/history/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
+from .models import Artist
 
 
 # Create your views here.
-    
 def artists(request):
-    return HttpResponse("Hello, world.  You're at the artist page!")
+    artist_list = Artist.objects.order_by('-artist_name')
+    output = ', '.join([a.artist_name for a in artist_list])
+    return HttpResponse(output)
 
 def detail(request, artist_id):
     return HttpResponse("Hello, world.  You're at the artist detail page!")
