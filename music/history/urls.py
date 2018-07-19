2d02f543ed72aa5d527423e4574476599aa0b00d
eagobert <eagobert@hotmail.com>
2018-07-19 14:09:04 -0500

    first migration YAAY

--- music/history/urls.py
+++ music/history/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('history/artists/details', views.details, name='details'),
-    path('history/artists', views.artists, name='artists'),
+    path('', views.index, name='index'),
+    path('', views.detail, name='detail'),
+    path('', views.artist, name='artist'),
 ]
--- music/history/views.py
+++ music/history/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world.  You're at the artist index!")
+    
+def artist(request, artist_id):
+    return HttpResponse("Hello, world.  You're at the artist page!")
+
+def detail(request, artist_id):
+    return HttpResponse("Hello, world.  You're at the artist detail page!")
