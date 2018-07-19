1563ae11f1a17fd5c4790c204656b4efa3c96356
eagobert <eagobert@hotmail.com>
2018-07-19 16:29:04 -0500

    complete artist view

--- music/history/views.py
+++ music/history/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Artist
 
 
 # Create your views here.
 def artists(request):
     artist_list = Artist.objects.order_by('-artist_name')
-    output = ', '.join([a.artist_name for a in artist_list])
-    return HttpResponse(output)
+    template = loader.get_template('history/artists.html')
+    context = {
+         'artist_list': artist_list,
+     }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, artist_id):
     return HttpResponse("Hello, world.  You're at the artist detail page!")
