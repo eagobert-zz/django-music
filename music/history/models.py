d1a56555cb4badb0b152911c0c9f094bdbbd560c
eagobert <eagobert@hotmail.com>
2018-08-02 15:52:46 -0500

    fix database

--- music/history/models.py
+++ music/history/models.py
@@ -2,17 +2,42 @@ from django.db import models
 
 # Create your models here.
 class Artist(models.Model):
+    """ Model represents a song's artist """
     artist_name = models.CharField(max_length=30)
     year_est = models.CharField(max_length=4)
 
-    # def __str__(self):
-    #     return 'Name: %s, Year Established: %s' % (self.artist_name, self.year_est)
+    def __str__(self):
+        return self.artist_name
+
+class Genre(models.Model):
+    """ Model represents an album's genre """
+    genre_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.genre_name
 
 class Song(models.Model):
+    """ Model represents an artist's song """
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, default="", on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     song_length = models.IntegerField(default=0)
     release_date = models.DateField(auto_now=False, auto_now_add=False)
 
-    # def __str__(self):
-    #     return 'Song Title: %s, Release Date: %s, Song Duration: %s' % (self.title, self.release_date, self.song_length )
+    def __str__(self):
+        return self.title
+
+class Album(models.Model):
+    """ Model represents an artist's album """
+    title = models.CharField(max_length=30)
+    label = models.CharField(max_length=30)
+    songs = models.ManyToManyField(Song, through='AlbumSongs')
+
+    def __str__(self):
+        return self.title
+
+
+class AlbumSongs(models.Model):
+    """ Model represents songs on an album """
+    album = models.ForeignKey(Album, on_delete= models.CASCADE)
+    songs = models.ForeignKey(Song, on_delete= models.CASCADE)
